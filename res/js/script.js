3e2cc4bc00717e1cedf258f9482a169cd81d67a7
JoosepAlbre <JoosepAlbre@users.noreply.github.com>
2020-10-23 19:57:14 +0300

    Added fetched info for posts with jquery

--- res/js/script.js
+++ res/js/script.js
@@ -1,3 +1,22 @@
-$(function() {
-    $("")
-})
+$(document).ready(function() {
+    $.getJSON("https://private-anon-8f6b2300ed-wad20postit.apiary-mock.com/posts", function(result) {
+        console.log(result)
+        var img_counter = 0;
+        var video_counter = 0;
+        $.each(result, function(i, info) {
+            $(".post-author img").eq(i).attr("src", info.author.avatar);
+            $(".author-name").eq(i).text(info.author.firstname + " " + info.author.lastname)
+            $(".date").eq(i).text(info.createTime);
+            if (!$.isEmptyObject(info.media)) {
+                if (info.media.type === "image") {
+                    $(".post-image img").eq(img_counter).attr("src", info.media.url);
+                    img_counter += 1;
+                } else if (info.media.type === "video") {
+                    $(".post-image video").eq(video_counter).attr("src", info.media.url);
+                    video_counter += 1;
+                }
+            }
+            $(".post-title h3").eq(i).text(info.text);
+        });
+    });
+});
