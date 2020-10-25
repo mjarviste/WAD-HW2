81d954b2a4346dd64da42e51402f9eee26c78ece
Mihkel Järviste <mjarviste@gmail.com>
2020-10-25 22:11:31 +0200

    Jquery for dropdown menu avatar

--- res/js/index.js
+++ res/js/index.js
@@ -20,3 +20,19 @@ $(document).ready(function() {
         });
     });
 });
+function show_hide() {
+    var click = document.getElementById("drop-content");
+    if(click.style.display === "none") {
+      click.style.display = "block";
+    } else {
+      click.style.display = "none";
+    }
+}
+
+$(document).ready(function() {
+    $.getJSON("https://private-anon-0432d73f9b-wad20postit.apiary-mock.com/users/1", function(result) {
+        console.log(result)
+        $(".dropdown-button img").attr("src", info.avatar);
+        $(".drop-content h3").text(info.firstname + " " + info.lastname + " " + info.email)
+    });
+});
